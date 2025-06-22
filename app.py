f408b98ba96e2efd595311c58e4108454a3a5083
Shrawan071 <don228336@gmail.com>
2025-06-22 23:29:54 +0545

    done

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Photo(db.Model):
+class todo(db.Model):
+    __tablename__ = 'todo'
     sno = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(150), nullable=False)
     description = db.Column(db.String(500), nullable=False)
@@ -15,8 +16,8 @@ class Photo(db.Model):
     date_time = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f"{self.task} {self.description} {self.status} {self.date_login}"
-    
+        return f"{self.task} {self.description} {self.status} {self.date_time}"
+
 @app.route("/", methods=['GET', 'POST'])
 def add_todo():
     if request.method == 'POST':
@@ -24,8 +25,38 @@ def add_todo():
         description = request.form.get('description')
         status = "Pending"
         if task and description:
-            new_entry = Photo(task=task, description=description,status=status)
+            new_entry = todo(task=task, description=description, status=status)
             db.session.add(new_entry)
             db.session.commit()
-    showing = Photo.query.all()
+    showing = todo.query.all()
     return render_template("index.html", showing=showing)
+
+@app.route("/show")
+def displaying():
+    showing = todo.query.all()
+    print(showing)
+    return "Data printed in console"
+
+@app.route("/delete/<int:sno>")
+def delete(sno):
+    todo_to_delete = todo.query.get_or_404(sno)
+    db.session.delete(todo_to_delete)
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/update/<int:sno>", methods=["GET", "POST"])
+def update(sno):
+    todo_item = todo.query.get_or_404(sno)
+    if request.method == "POST":
+        todo_item.task = request.form.get("task")
+        todo_item.description = request.form.get("description")
+        todo_item.status = request.form.get("status")
+        db.session.commit()
+        return redirect("/")
+    return render_template("update.html", todo=todo_item)
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
